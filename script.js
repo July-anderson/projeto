d33ec24a55f70cacdac950fef633f08e6e10cd0f
Dexter <julyanderson5421@gmail.com>
2025-07-09 21:12:19 -0300

    :tada: Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+
+  // pegar a tag img
+
+  // subistitui a imagem
+  // se tiver ligth mode, adicionar a imagem light
+  // se tiver sem light mode, manter a imagem normal
+}
